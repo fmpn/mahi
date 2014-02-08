1a0d2136fad30364faa83e2621e031d9830c4d96
guilhermepaiva <guilhermepaiva9@gmail.com>
2014-02-08 18:18:15 -0300

    integração com bootsrap e alterações no route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Mahi::Application.routes.draw do
   devise_for :users
   resources :services
 
+  root :to => "services#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
