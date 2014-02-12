c08bd69b7a5df6c727ba259c0bb88c4abc880083
guilhermepaiva <guilhermepaiva9@gmail.com>
2014-02-11 22:11:22 -0300

    controller de services alterado para suportar a criação de vários serviços

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -3,53 +3,60 @@ class ServicesController < ApplicationController
 
   # GET /services
   # GET /services.json
+
+  # comentado por enquanto before_filter :authenticate_user!
   def index
-    @services = Service.all
+    @user = User.find(params[:user_id])
+    @services = @user.services
+    authorize! :manage, @services
   end
 
   # GET /services/1
   # GET /services/1.json
   def show
+    @service = Service.find(params[:id])
+    @user = User.find(@service.user_id)
+    authorize! :manage, @service
   end
 
   # GET /services/new
   def new
-    @service = Service.new
+    @user = User.find(params[:user_id])
+    @service = @user.services.buid
+    authorize! :manage, @service
   end
 
   # GET /services/1/edit
   def edit
+    @service = Service.find(params[:id])
   end
 
   # POST /services
   # POST /services.json
   def create
-    @service = Service.new(service_params)
+    @user = User.find(params[:user_id])
+      @service = @user.services.create(serv_params)
+      @service.user_id = current_user.id
+      authorize! :manage, @service
 
-    respond_to do |format|
       if @service.save
-        format.html { redirect_to @service, notice: 'Service was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @service }
+        redirect_to @service, notice: 'Servico criado com sucesso!' 
       else
-        format.html { render action: 'new' }
-        format.json { render json: @service.errors, status: :unprocessable_entity }
+        render action: 'new' 
       end
     end
-  end
 
   # PATCH/PUT /services/1
   # PATCH/PUT /services/1.json
   def update
-    respond_to do |format|
+    @service = Service.find(params[:id])
+    authorize! :manage, @service
       if @service.update(service_params)
-        format.html { redirect_to @service, notice: 'Service was successfully updated.' }
-        format.json { head :no_content }
+        redirect_to @service, notice: 'Servico atualizado com sucesso!'
       else
-        format.html { render action: 'edit' }
-        format.json { render json: @service.errors, status: :unprocessable_entity }
+        render action: 'edit'
       end
     end
-  end
 
   # DELETE /services/1
   # DELETE /services/1.json
@@ -62,6 +69,10 @@ class ServicesController < ApplicationController
   end
 
   private
+    def serv_params
+      params.require(:service).permit(:name, :email)
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_service
       @service = Service.find(params[:id])
